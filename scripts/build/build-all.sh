5c4f55ea4c3ca7615cd9a3bde32a130b4ef2bc7c
Drew Short <warrick@sothr.com>
2015-08-31 00:36:13 +0000

    Fix misnamed files in the build-all script

--- scripts/build/build-all.sh
+++ scripts/build/build-all.sh
@@ -5,9 +5,9 @@
 set -x
 set -e
 
-: ${BUILD_SNAPSHOT_SCRIPT:=build_snap.sh}
-: ${BUILD_RUST_SCRIPT:=build_rust.sh}
-: ${BUILD_CARGO_SCRIPT:=build_cargo.sh}
+: ${BUILD_SNAPSHOT_SCRIPT:=~/build-snap.sh}
+: ${BUILD_RUST_SCRIPT:=~/build-rust.sh}
+: ${BUILD_CARGO_SCRIPT:=~/build-cargo.sh}
 : ${BASH_SHELL:=/usr/bin/env bash}
 #Defaults to the nightly branch
 : ${CHANNEL:=nightly}
