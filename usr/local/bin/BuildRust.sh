f379f195555ad5df2701750b9b3c3e336badb388
Drew Short <warrick@sothr.com>
2015-09-03 18:19:24 +0000

    Update to the systemd build script to reduce the noise in the ouput logs and modularize the closing of containers that dodn't exit nicely

--- usr/local/bin/BuildRust.sh
+++ usr/local/bin/BuildRust.sh
@@ -3,15 +3,40 @@
 # Wonderful build script to run nightly
 # This is what builds our rust binaries
 
+# Check the systemd machinectl to see if a machine is running
+is_machine_running () {
+  local machine_registered="$(machinectl list | grep -F $1 | sed 's/^\s*$1\s.*$/$1/')"
+  if [ -z machine_registered ]; then
+    return true
+  else
+    return false
+  fi 
+}
+
+# Stop a machine if it is already running
+stop_running_machine () {
+  local already_running=is_machine_running $1
+  if already_running; then
+    machinectl terminate $1
+  fi
+}
+
 set -x
 
-# Build nightly, beta, and then stable
+# Build process start time
 start_time="$(date +%s)"
-machinectl terminate RustBuild
-systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh || machinectl terminate RustBuild
-systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh beta || machinectl terminate RustBuild
-systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh stable || machinectl terminate RustBuild
-machinectl terminate RustBuild
+
+# Request an already running instance terminate
+stop_running_machine "RustBuild"
+
+# Build nightly, beta, and then stable in the same container consecutively
+systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh || stop_running_machine "RustBuild"
+systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh beta || stop_running_machine "RustBuild"
+systemd-nspawn -D /chroots/RustBuild /bin/bash /root/build-all.sh stable || stop_running_machine "RustBuild"
+
+# Make sure we've exited the container at the end
+stop_running_machine "RustBuild"
+
 end_time="$(date +%s)"
 running_time="$((end_time-start_time))"
 # Prints Hours:Minutes:Seconds
