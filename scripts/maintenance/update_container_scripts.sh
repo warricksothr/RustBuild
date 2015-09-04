b05d1e2ba23e0e61d2776fcde9a82a52e4dfec12
Drew Short <warrick@sothr.com>
2015-09-04 14:03:11 +0000

    Fixed the deletion part of the build_rust process. Added a maintenance script folder and script for updating all containers.

---
+++ scripts/maintenance/update_container_scripts.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#
+# This script will walk through the containers and update their repositories
+# to the latest RustBuild version.
+#
+
+set -x
+set -e
+
+: ${CHROOT_DIR:=/chroots}
+
+if [ ! -z $1 ]; then
+  CHROOT_DIR="$1"
+fi
+
+directories=($(ls -d ${CHROOT_DIR}/*))
+
+for file in ${directories[@]}; do
+  if [ -d $file ]; then
+    container=${file}
+    container_root=${container}/root
+    rust_build_dir=${container_root}/RustBuild
+    if [ -d $rust_build_dir ]; then
+      opwd=$PWD
+      cd $rust_build_dir
+      git clean -df
+      git checkout -- .
+      git pull
+      cd $opwd
+    fi
+  fi
+done
