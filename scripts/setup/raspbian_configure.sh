af0f0e34e615078d62b82018279aa7752e96ce74
Drew Short <warrick@sothr.com>
2015-09-05 19:31:44 +0000

    #1 Removed the auto download of the snapshot.

--- scripts/setup/raspbian_configure.sh
+++ scripts/setup/raspbian_configure.sh
@@ -46,16 +46,6 @@ bash dropbox_uploader.sh
 # Make sure our dropbox root directory exists
 bash dropbox_uploader.sh mkdir ${CONTAINER_TAG}
 
-# Try to Download and Upload the first ARMv6 compatible snapshot
-curl -L "https://www.dropbox.com/s/ss9gaxvhosko7y7/rust-stage0-2015-04-27-857ef6e-linux-arm-5f6b8f68b46e8229a88476f8a3eda676903f0fbb.tar.bz2\?dl\=0" -o rust-stage0-2015-04-27-857ef6e-linux-arm-5f6b8f68b46e8229a88476f8a3eda676903f0fbb.tar.bz2 || true
-if [ -f "rust-stage0-2015-04-27-857ef6e-linux-arm-5f6b8f68b46e8229a88476f8a3eda676903f0fbb.tar.bz2" ]; then
-  bash dropbox_uploader.sh mkdir "${CONTAINER_TAG}/snapshots"
-  bash dropbox_uploader.sh upload "rust-stage0-2015-04-27-857ef6e-linux-arm-5f6b8f68b46e8229a88476f8a3eda676903f0fbb.tar.bz2" "${CONTAINER_TAG}/snapshots"
-  rm "rust-stage0-2015-04-27-857ef6e-linux-arm-5f6b8f68b46e8229a88476f8a3eda676903f0fbb.tar.bz2"
-else
-  echo "You will need to copy your own snapshot to the ${CONTAINER_TAG}/snapshots directory"
-fi
-
 # Build OpenSSL with the required information for use in building cargo
 cd $OPENSSL_SRC
 ./config -fPIC shared --prefix=$OPENSSL_DIR/dist
