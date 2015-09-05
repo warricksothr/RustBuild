81851ffcab04cb44333fb73e08b14b3cdf55d8e7
Drew Short <warrick@sothr.com>
2015-09-05 19:45:02 +0000

    #1 Didn't have the right binary operator when checking for the wheezy key

--- scripts/setup/raspbian_root_setup.sh
+++ scripts/setup/raspbian_root_setup.sh
@@ -45,7 +45,7 @@ if [ ! -f raspbian.public.key ]; then
   wget http://archive.raspbian.org/raspbian.public.key
 fi
  
-if [ ~ -f archive-key-7.0.asc ]; then
+if [ ! -f archive-key-7.0.asc ]; then
   wget https://ftp-master.debian.org/keys/archive-key-7.0.asc
 fi
 
